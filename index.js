64b462fbbf9e35dffe637dde0f0f2242e5539efa
shawukie4them <shawukiekawuki@gmail.com>
2024-07-01 13:30:47 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+//Excercise 1
+function saturdayFun(activity ='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//Exercise 2
+ mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+//Exercise 3
+function wrapAdjective(flair = '*'){
+    return function(adjective = 'special'){
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
